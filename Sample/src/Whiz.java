9b1b275af65499ccaf04626ee7e3c44fd5f87d23
Vishnu P <Vishnu P@Vishnu>
2017-11-12 00:04:13 +0530

    Signed-off-by: Vishnu P <Vishnu P@Vishnu>

---
+++ Sample/src/Whiz.java
@@ -0,0 +1,20 @@
+
+public class Whiz {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String out = "0";
+		String s = "1";
+		int x = 7;
+		if(x>5)
+			s=out+"2";
+		else
+			out+=9;
+		System.out.println(out);
+		System.out.println(s);
+		
+
+	}
+
+}
